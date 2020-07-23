d7ed85860ca9da00193afc03905c54ca69666327
hardpenguin <hardpenguin13@gmail.com>
2020-07-23 14:12:59 +0200

    updated the list of digital stores; tiny update to log message in the console

--- kaufer.py
+++ kaufer.py
@@ -5,7 +5,7 @@ class Kaufer(object):
     def Search(query, search_links):
         print("Searching on:")
         for store, link in search_links.items():
-            print("%s" % store, end=" ")
+            print("%s" % store, end=" | ")
             l = link.replace("{{ query }}", query)
             os.system("xdg-open \"%s\" " % l)
         print("")
--- stores.py
+++ stores.py
@@ -6,8 +6,14 @@ search_links = OrderedDict([
     ("Humble Store","https://www.humblebundle.com/store/search?search={{ query }}"),
     ("Steam", "https://store.steampowered.com/search/?term={{ query }}"),
     ("Google Play", "https://play.google.com/store/search?q={{ query }}&c=apps"),
-    ("Discord", "https://discordapp.com/store/browse?q={{ query }}"),
     ("Kartridge", "https://www.kartridge.com/search/{{ query }}"),
     ("iTunes", "https://www.google.com/search?q=site%3Aitunes.apple.com+{{ query }}"),
+    ("Epic Games Store", "https://www.epicgames.com/store/browse?q={{ query }}"),
+    ("Origin", "https://www.origin.com/search?searchString={{ query }}"),
+    ("Ubisoft Store", "https://store.ubi.com/search?q={{ query }}"),
+    ("Nintendo Switch", "https://www.nintendo.com/search/#category=game&query={{ query }}"),
+    ("PlayStation Store", "https://store.playstation.com/en-us/grid/search-game/1?query={{ query }}"),
+    ("Microsoft Store", "https://www.microsoft.com/en-us/search/shop/games?q={{ query }}"),
+    ("Bethesda.net", "https://bethesda.net/search?q={{ query }}&scope=games")
     ])
 # you can comment out the stores you don't want to query
